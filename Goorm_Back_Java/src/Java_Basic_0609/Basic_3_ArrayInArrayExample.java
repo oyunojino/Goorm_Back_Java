0e48f6b6af5e35073bdd8ab140116eecdcf9d488
oyunojin <dusrn2xlayjoh@gmail.com>
2023-06-09 18:17:08 +0900

    Basic_3_ArrayInArrayExample

---
+++ Goorm_Back_Java/src/Java_Basic_0609/Basic_3_ArrayInArrayExample.java
@@ -0,0 +1,32 @@
+package Java_Basic_0609;
+
+public class Basic_3_ArrayInArrayExample {
+    public static void main() {
+        int[][] mathScores = new int[2][3];
+        for(int i=0; i<mathScores.length; i++) {
+            for(int k=0; k<mathScores[i].length; k++) {
+                System.out.println("mathScores["+i+"]["+k+"]="
+                        +mathScores[i][k]);
+            }
+        }
+        System.out.println();
+
+        int[][] englishScores = new int[2][];
+        englishScores[0] = new int[2];
+        englishScores[1] = new int[3];
+        for(int i=0; i<englishScores.length; i++) {
+            for(int k=0; k<englishScores[i].length; k++) {
+                System.out.println("englishScores["+i+"]["+k+"]=" + englishScores[i][k]);
+            }
+        }
+        System.out.println();
+
+        int[][] javaScores = { {95, 80}, {92, 96, 80} };
+        for(int i=0; i<javaScores.length; i++) {
+            for(int k=0; k<javaScores[i].length; k++) {
+                System.out.println("javaScores["+i+"]["+k+"]="
+                        +javaScores[i][k]);
+            }
+        }
+    }
+}
--- Goorm_Back_Java/src/Main.java
+++ Goorm_Back_Java/src/Main.java
@@ -1,10 +1,14 @@
 import Java_Basic_0609.Basic_1_Calc_Main;
+import Java_Basic_0609.Basic_3_ArrayInArrayExample;
 
 public class Main {
     public static void main(String[] args) {
         Basic_1_Calc_Main calcMain = new Basic_1_Calc_Main();
-        calcMain.main();
+//        calcMain.main();
 
+        Basic_3_ArrayInArrayExample arrayMain = new Basic_3_ArrayInArrayExample();
+//        arrayMain.main();
 
+        
     }
 }
