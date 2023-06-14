a35eb42805b8643f2ee9e00f1a240b15b2703cc2
oyunojin <dusrn2xlayjoh@gmail.com>
2023-06-14 10:42:20 +0900

    promotion_ 자동형변환 예제

---
+++ Goorm_Back_Java/src/Java_Basic_0914/Basic_1_Promotion.java
@@ -0,0 +1,10 @@
+package Java_Basic_0914;
+
+public class Basic_1_Promotion {
+    public static void main() {
+        int aVar = 10;
+        float bVar = 10.5f;
+
+        System.out.println("aVar * bVar = " + aVar*bVar);
+    }
+}
---
+++ Goorm_Back_Java/src/Java_Basic_0914/Main.java
@@ -0,0 +1,8 @@
+package Java_Basic_0914;
+
+public class Main {
+    public static void main(String[] args) {
+        Basic_1_Promotion promotion = new Basic_1_Promotion();
+        promotion.main();
+    }
+}
