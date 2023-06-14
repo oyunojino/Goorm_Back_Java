87ada3d7894413787393ecf08fac2929e7505033
oyunojin <dusrn2xlayjoh@gmail.com>
2023-06-14 12:00:21 +0900

    datatype 관련 실습

---
+++ Goorm_Back_Java/src/Java_Basic_0914/Basic_2_datatype.java
@@ -0,0 +1,18 @@
+package Java_Basic_0914;
+
+public class Basic_2_datatype {
+    public static void main() {
+        char aVar = 'A';
+        System.out.println("aVar : " + aVar);
+        System.out.println("aVar : " + (int)aVar);
+
+        boolean bVar = true;
+        System.out.println(bVar);
+
+        String str1 = "HELLO";
+        String str2 = "WELCOME";
+
+        System.out.println("str1 : " + str1);
+        System.out.println("str2 : " + str2);
+    }
+}
