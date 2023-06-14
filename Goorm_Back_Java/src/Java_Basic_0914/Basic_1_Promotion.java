6b2e4dccc80c06f4cf4081694f0bda385487ee0f
oyunojin <dusrn2xlayjoh@gmail.com>
2023-06-14 12:00:45 +0900

    주석 추가 및 수정

--- Goorm_Back_Java/src/Java_Basic_0914/Basic_1_Promotion.java
+++ Goorm_Back_Java/src/Java_Basic_0914/Basic_1_Promotion.java
@@ -2,8 +2,8 @@ package Java_Basic_0914;
 
 public class Basic_1_Promotion {
     public static void main() {
-        int aVar = 10;
-        float bVar = 10.5f;
+        int aVar = 10;      // 정수
+        float bVar = 10.5f; // 실수
 
         System.out.println("aVar * bVar = " + aVar*bVar);
     }
--- Goorm_Back_Java/src/Java_Basic_0914/Main.java
+++ Goorm_Back_Java/src/Java_Basic_0914/Main.java
@@ -3,6 +3,12 @@ package Java_Basic_0914;
 public class Main {
     public static void main(String[] args) {
         Basic_1_Promotion promotion = new Basic_1_Promotion();
-        promotion.main();
+//        promotion.main();
+
+        Basic_2_datatype datatype = new Basic_2_datatype();
+//        datatype.main();
+
+        Basic_3_operator operator = new Basic_3_operator();
+        operator.main();
     }
 }
