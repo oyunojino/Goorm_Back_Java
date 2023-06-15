2fb746002e31930d62bd1d394c207080fccc6e60
oyunojin <dusrn2xlayjoh@gmail.com>
2023-06-15 11:21:09 +0900

    배열 관련 예제

--- Goorm_Back_Java/src/JAVA_Goorm/G_01_array.java
+++ Goorm_Back_Java/src/JAVA_Goorm/G_01_array.java
@@ -2,7 +2,7 @@ package JAVA_Goorm;
 
 public class G_01_array {
 
-    public static void main()  {
+    public static void main1() {
         int[][] mathScores = new int[2][3];
         for(int i=0; i<mathScores.length; i++) {
             for(int k=0; k<mathScores[i].length; k++) {
@@ -28,6 +28,40 @@ public class G_01_array {
                 System.out.println("javaScores["+i+"]["+k+"]="
                         +javaScores[i][k]);
             }
+        }
+    }
+
+    public static void main2() {
+        String[] strArray = new String[3];
+        strArray[0] = "Java";
+        strArray[1] = "Java";
+        strArray[2] = new String("Java");
+        System.out.println( strArray[0] == strArray[1]);
+        System.out.println( strArray[0] == strArray[2] );
+        System.out.println( strArray[0].equals(strArray[2]) );
+    }
+
+    public static void main3() {
+        int[] oldIntArray = { 1, 2, 3 };
+        int[] newIntArray = new int[5];
+
+        for(int i=0; i<oldIntArray.length; i++) {
+            newIntArray[i] = oldIntArray[i];
+        }
+
+        for(int i=0; i<newIntArray.length; i++) {
+            System.out.print(newIntArray[i] + ", ");
+        }
+    }
+
+    public static void main4() {
+        String[] oldStrArray = { "java", "array", "copy" };
+        String[] newStrArray = new String[5];
+
+        System.arraycopy( oldStrArray, 0, newStrArray, 0, oldStrArray.length);
+
+        for(int i=0; i<newStrArray.length; i++) {
+            System.out.print(newStrArray[i] + ", ");
         }
     }
 
